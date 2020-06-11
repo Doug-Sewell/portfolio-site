492fe9c47b62e84456269c85c1ec4b7bd7ea3c0f
Doug Sewell <DouglasASewell@gmail.com>
2020-06-10 22:17:30 -0400

    Got started on templating the Contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Card from '../../UI/Card/Card';
+import classes from './contact.module.css';
+import { Icon } from '@material-ui/core';
 
-const contact = () => <h1>Contact Doug</h1>
+const contact = () =>
+    <div className={classes.ContactContainer}>
+        <Card>
+            <h1>Let's get in touch</h1>
+        </Card>
+    </div>
 
 export default contact;
